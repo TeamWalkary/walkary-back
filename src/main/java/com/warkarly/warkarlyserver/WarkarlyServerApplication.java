b089fef592fe458d6a8f5b3d215e834087bbef33
meengi <meengi@korpay.com>
2023-10-21 15:17:09 +0900

    first

---
+++ src/main/java/com/warkarly/warkarlyserver/WarkarlyServerApplication.java
@@ -0,0 +1,13 @@
+package com.warkarly.warkarlyserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarkarlyServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarkarlyServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/warkarly/warkarlyserver/WarkarlyServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.warkarly.warkarlyserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarkarlyServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
