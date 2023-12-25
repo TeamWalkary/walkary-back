2a2dba20409724c6cee1d933797adf836dd62e6b
reheat <reheat1540@gmail.com>
2023-12-25 19:52:31 +0900

    fix: 해당 월에 일기 또는 핀이 없을 때 거르는 기능 제외(빈 값을 보내는 걸로 결정)

--- src/main/java/com/walkary/service/CalendarService.java
+++ src/main/java/com/walkary/service/CalendarService.java
@@ -29,10 +29,6 @@ public class CalendarService {
         List<Integer> diaryDayList = diaryRepository.findByMonth(year, month, userId);
         List<Integer> pointMapDayList = pointMapRepository.findByMonth(year, month, userId);
 
-        if (diaryDayList == null && pointMapDayList == null) {
-            throw new IllegalArgumentException("해당 월엔 일기 또는 핀을 작성하지 않으셨습니다");
-        }
-
         String diaryDays = diaryDayList.stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining(", "));
